9d7948c6d8bf6220483b460ddf840f920307b132
BrycePearce <bpearce@angelo.edu>
2017-05-20 18:07:58 -0500

    Added slider for demo, moved some things around

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
 import React, { Component } from 'react';
+import { render } from 'react-dom';
 import logo from './logo.svg';
+import ProgressBar from './ProgressBar.js';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      min: 0,
+      max: 100,
+      current: 0
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(change) {
+    console.log(change.target.value);
+    this.setState({ current: change.target.value });
+  }
+
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
         </div>
-        <div className="myProgress">
-          <div className="myBar">0%</div>
+        <div className = "currentValue"> {this.state.current} </div>
+        <div className="sliderContainer">
+          <input id="slider" type="range" value={this.state.current} min={this.state.min} max={this.state.max} step="1" onChange={this.handleChange} />
         </div>
+        <ProgressBar />
       </div>
     );
   }
---
+++ src/ProgressBar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './ProgressBar.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="myProgress">
+        <div className="myBar">0%</div>
+      </div>
+    );
+  }
+}
+
+export default App;
