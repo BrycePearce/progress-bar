ffc51c0fc13573b89e4297b03d6f89fa4c02348f
BrycePearce <bpearce@angelo.edu>
2017-05-20 17:06:24 -0500

    forked boilerplate, added progress bar html/css

---
+++ progress.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { render } from 'react-dom';
+class Progress extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, world</h1>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('root')
+);
