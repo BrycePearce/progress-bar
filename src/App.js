27361c9363c83c4c02dc2af15415c6f871fe41f8
BrycePearce <bpearce@angelo.edu>
2017-05-20 19:45:53 -0500

    Fixed some CSS, added reactive components. Working fully now

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
     this.state = {
       min: 0,
       max: 100,
-      current: 0
+      current: 50
     };
 
     this.handleChange = this.handleChange.bind(this);
@@ -29,11 +29,11 @@ class App extends Component {
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
         </div>
-        <div className = "currentValue"> {this.state.current} </div>
+        <div className="currentValue"> {this.state.current} </div>
         <div className="sliderContainer">
-          <input id="slider" type="range" value={this.state.current} min={this.state.min} max={this.state.max} step="1" onChange={this.handleChange} />
+          <input className="slider" type="range" value={this.state.current} min={this.state.min} max={this.state.max} step="1" onChange={this.handleChange} />
         </div>
-        <ProgressBar />
+        <ProgressBar min={this.state.min} max={this.state.max} current={this.state.current} />
       </div>
     );
   }
--- src/ProgressBar.js
+++ src/ProgressBar.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
-import './ProgressBar.css';
+
 
 class App extends Component {
   render() {
+    const divStyle = {
+      width: this.props.current + "%",
+      height: 30 + 'px',
+      backgroundColor: '#4CAF50',
+      textAlign: 'center',
+      lineHeight: 30 + 'px',
+      color: 'white'
+    }
     return (
       <div className="myProgress">
-        <div className="myBar">0%</div>
+        <div className="myBar" style={divStyle}>{this.props.current + "%"}</div>
       </div>
     );
   }
